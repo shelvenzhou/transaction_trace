849bc917268928906c2f434220546864f676e1f0
Shelven Zhou <favoring.zhou49@gmail.com>
2018-12-29 15:01:17 +0800

    add table creation

--- ethereum_database.py
+++ ethereum_database.py
@@ -1,56 +1,48 @@
 from google.cloud import bigquery
 import sqlite3
 from datetime import datetime
+import decimal
 import logging
 
 l = logging.getLogger("bigquery-ethereum-crawler.ethereum_database")
 
-DB_FILEPATH = "bigquery_ethereum_traces.sqlite3"
+DB_FILEPATH = "bigquery_ethereum.sqlite3"
 
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
-SCHEME = {
-    'transaction_hash':     "STRING",
-    'transaction_index':    "INTEGER",
-    'from_address':         "STRING",
-    'to_address':           "STRING",
-    'value':                "INTEGER",
-    'input':                "STRING",
-    'output':               "STRING",
-    'trace_type':           "STRING",
-    'call_type':            "STRING",
-    'reward_type':          "STRING",
-    'gas':                  "INTEGER",
-    'gas_used':             "INTEGER",
-    'subtraces':            "INTEGER",
-    'trace_address':        "STRING",
-    'error':                "STRING",
-    'status':               "INTEGER",
-    'block_timestamp':      "TIMESTAM",
-    'block_number':         "INTEGER",
-    'block_hash':           "STRING",
-}
+
+def time_to_str(t):
+    return t.strftime(DATETIME_FORMAT)
+
+
+def str_to_time(s):
+    return datetime.strptime(s, DATETIME_FORMAT)
+
+
+def adapt_decimal(d):
+    return str(d)
+
+
+def convert_decimal(s):
+    return decimal.Decimal(s)
+
+
+sqlite3.register_adapter(decimal.Decimal, adapt_decimal)
+sqlite3.register_converter("DECIMAL", convert_decimal)
 
 
 class EthereumDatabase(object):
 
     def __init__(self, db_filepath=DB_FILEPATH):
         self.db_filepath = db_filepath
-        self.conn = sqlite3.connect(self.db_filepath)
+        self.conn = sqlite3.connect(self.db_filepath, detect_types=sqlite3.PARSE_DECLTYPES)
         self.conn.row_factory = sqlite3.Row
+        self.cur = self.conn.cursor()
 
         self.client = bigquery.Client()
 
     def __del__(self):
         self.conn.close()
-
-    @staticmethod
-    def time_to_str(timestamp):
-        return timestamp.strftime(DATETIME_FORMAT)
-
-    @staticmethod
-    def str_to_time(string):
-        return datetime.strptime(string, DATETIME_FORMAT)
 
     def get_ethereum_data(self, from_time, to_time):
         query_str = (
@@ -60,10 +52,41 @@ class EthereumDatabase(object):
 
         return self.client.query(query_str).result()
 
+    def database_create(self):
+        self.cur.execute("""
+            CREATE TABLE traces(
+                transaction_hash TEXT,
+                transaction_index INT,
+                from_address TEXT,
+                to_address TEXT,
+                value DECIMAL,
+                input TEXT,
+                output TEXT,
+                trace_type TEXT NOT NULL,
+                call_type TEXT,
+                reward_type TEXT,
+                gas INT,
+                gas_used INT,
+                subtraces INT,
+                trace_address TEXT,
+                error TEXT,
+                status INT,
+                block_timestamp TIMESTAMP NOT NULL,
+                block_number INT NOT NULL,
+                block_hash STRING NOT NULL
+            );
+            """)
 
-    def insert_to_database(self, rows):
-        with self.conn.cursor() as c:
-            c.executemany("INSERT INTO traces(transaction_hash, transaction_index, from_address, to_address, value, input, output, trace_type, call_type, reward_type, gas, gas_used, subtraces, trace_address, error, status, block_timestamp, block_number, block_hash) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", rows)
+        self.database_commit()
+
+    def database_insert(self, rows):
+        self.cur.executemany("""
+            INSERT INTO traces(transaction_hash, transaction_index, from_address, to_address, value, input, output, trace_type, call_type, reward_type, gas, gas_used, subtraces, trace_address, error, status, block_timestamp, block_number, block_hash)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+            """, rows)
+
+    def database_commit(self):
+        self.conn.commit()
 
     def read_from_database(self):
         '''
@@ -71,7 +94,7 @@ class EthereumDatabase(object):
             'transaction_index':    INTEGER,    NULLABLE
             'from_address':         STRING,     NULLABLE
             'to_address':           STRING,     NULLABLE
-            'value':                INTEGER,    NULLABLE
+            'value':                DECIMAL,    NULLABLE
             'input':                STRING,     NULLABLE
             'output':               STRING,     NULLABLE
             'trace_type':           STRING,     REQUIRED
