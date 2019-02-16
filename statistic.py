a28a914c823c10b98570c0888b1924ee40ace0fc
stillkam <stillkammy@gmail.com>
2019-02-16 19:27:34 +0800

    build graph when poor

--- statistic.py
+++ statistic.py
@@ -43,8 +43,9 @@ class Statistic(object):
         m = hashlib.sha256(str(symbolic_subtraces).encode('utf-8'))
         return '0x' + m.hexdigest()
 
-    def build_trace_graph(self, graph=None, from_time=None, to_time=None):
-        tx2hash = {}
+    def build_trace_graph(self, graph=None, tx2hash=None, from_time=None, to_time=None):
+        if tx2hash == None:
+            tx2hash = {}
         if graph == None:
             trace_graph = nx.DiGraph()
         else:
@@ -77,61 +78,95 @@ class Statistic(object):
             tx2hash[tx_hash]['countnow'] += 1
 
             if tx2hash[tx_hash]['countnow'] == tx2hash[tx_hash]['subtraces_count']:
-                tx2hash[tx_hash]['subtraces_hash'] = self.hash_subtraces(tx2hash[tx_hash]['subtraces'])
+                subtraces_hash = self.hash_subtraces(tx2hash[tx_hash]['subtraces'])
                 for subtrace in tx2hash[tx_hash]['subtraces']:
                     from_address = subtrace[1]
                     to_address = subtrace[2]
-                    subtraces_hash = tx2hash[tx_hash]['subtraces_hash']
                     trace_graph.add_edge(from_address, to_address)
-
                     for addr in (from_address, to_address):
                         if subtraces_hash not in trace_graph.node[addr]:
                             trace_graph.node[addr][subtraces_hash] = []
                         if tx_hash not in trace_graph.node[addr][subtraces_hash]:
                             trace_graph.node[addr][subtraces_hash].append(tx_hash)
 
-                tx2hash[tx_hash]['subtraces'] = None
+                tx2hash[tx_hash] = subtraces_hash
 
             count += 1
             sys.stdout.write(str(count) + '\r')
             sys.stdout.flush()
 
         print(len(tx2hash.keys()), "transactions")
-        return trace_graph
+        return (trace_graph, tx2hash)
 
-    def build_trace_graph_on_multidb(self, from_time, to_time):
+    def build_trace_graph_on_multidb(self, from_time, to_time, tx2hash=None):
         date = from_time.date()
         trace_graph = None
         while date <= to_time.date():
             print(date_to_str(date))
             self.local = EthereumDatabase(f"{self.db_path}bigquery_ethereum_{date_to_str(date)}.sqlite3")
-            trace_graph = self.build_trace_graph(graph=trace_graph)
+            (trace_graph, tx2hash) = self.build_trace_graph(graph=trace_graph, tx2hash=tx2hash)
             date += timedelta(days=1)
             gc.collect()
-        return trace_graph
+        return (trace_graph, tx2hash)
 
-    def analyze_nodes(self, trace_graph):
-        tx_attr = {}
-        node_attr = {}
+    def build_graph_when_poor(self, from_time, to_time):
+        start = from_time
+        end = from_time + timedelta(days=6)
+        tx_attr = node_attr = hash2tx = tx2hash = None
+        while start < to_time:
+            (trace_graph, tx2hash) = self.build_trace_graph_on_multidb(start, end)
+            (tx_attr, node_attr, hash2tx) = self.extract_from_graph(trace_graph, tx_attr, node_attr, hash2tx)
+            trace_graph = None
+            gc.collect()
+            start = end + timedelta(days=1)
+            end = timedelta(days=6)
+            if end > to_time:
+                end = to_time
+
+        self.analyze(tx_attr, node_attr, hash2tx, tx2hash)
+
+    def extract_from_graph(self, trace_graph, tx_attr=None, node_attr=None, hash2tx=None):
+        if tx_attr == None and node_attr == None:
+            tx_attr = {}
+            node_attr = {}
+            hash2tx = {}
         nodes = trace_graph.nodes(data=True)
         for node in nodes:
             node_addr = node[0]
-            node_attr[node_addr] = []
+            if node_addr not in node_attr:
+                node_attr[node_addr] = {}
             hash_count = {}
-            max_count = 0
             for h in node[1]:
                 hash_count[h] = len(node[1][h])
-                if hash_count[h] > max_count:
-                    max_count = hash_count[h]
             for h in node[1]:
-                ratio = max_count/hash_count[h]
-                node_attr[node_addr].append(hash_count[h])
+                if h not in node_attr[node_addr]:
+                    node_attr[node_addr][h] = hash_count[h]
+                else:
+                    node_attr[node_addr][h] += hash_count[h]
                 for tx in node[1][h]:
                     if tx not in tx_attr:
-                        tx_attr[tx] = []
-                    tx_attr[tx].append(ratio)
+                        tx_attr[tx] = {}
+                    tx_attr[tx][node_addr] = None
 
-        return (tx_attr, node_attr)
+        return (tx_attr, node_attr, hash2tx)
+
+    def analyze(self, tx_attr, node_attr, hash2tx, tx2hash):
+        max_hash = {}
+        for node_addr in node_attr:
+            max_count = 0
+            for h in node_attr[node_addr]:
+                if node_attr[node_addr][h] > max_count:
+                    max_count = node_attr[node_addr][h]
+            max_hash[node_addr] = max_count
+        
+        for tx in tx_attr:
+            for node_addr in tx_attr[tx]:
+                h = tx2hash[tx]
+                import IPython;IPython.embed()
+                tx_attr[tx][node_addr] = max_hash[node_addr]/node_attr[node_addr][h]
+
+        import IPython;IPython.embed()
+        
 
 def main():
     from_time = datetime(2018, 10, 7, 0, 0, 0)
@@ -139,14 +174,18 @@ def main():
     analyzer = Statistic(date, DB_PATH)
 
     print("Statistic analysis on", date_to_str(date))
-    trace_graph = analyzer.build_trace_graph()
-    (tx_attr, node_attr) = analyzer.analyze_nodes(trace_graph)
+    (trace_graph, tx2hash) = analyzer.build_trace_graph()
+    (tx_attr, node_attr, hash2tx) = analyzer.extract_from_graph(trace_graph)
+    import IPython;IPython.embed()
+    trace_graph = None
+    gc.collect()
+    analyzer.analyze(tx_attr, node_attr, hash2tx, tx2hash)
 
     # to_time = datetime(2018, 10, 7, 0, 0, 0)
     # print("Statistic analysis from", date_to_str(from_time.date()), "to", date_to_str(to_time.date()))
-    # trace_graph = analyzer.build_trace_graph_on_multidb(from_time, to_time)
+    # (trace_graph, tx2hash) = analyzer.build_trace_graph_on_multidb(from_time, to_time)
 
-    import IPython;IPython.embed()
+    # import IPython;IPython.embed()
 
 if __name__ == "__main__":
     main()
