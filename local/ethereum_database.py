8a74e80e7730adaa4275a40158a2c537c98b7d20
Shelven Zhou <favoring.zhou49@gmail.com>
2019-03-05 10:02:28 +0800

    refactor the opertion interface of subtraces table
    remove unused crawl_records table
    avoid reusing Cursor for some unexpected side-effects
    rename internal fields

--- local/ethereum_database.py
+++ local/ethereum_database.py
@@ -6,15 +6,9 @@ import datetime
 
 l = logging.getLogger("bigquery-ethereum-crawler.local.ethereum_database")
 
-DB_FILEPATH = "bigquery_ethereum.sqlite3"
-
 
 def adapt_decimal(d):
     return str(d)
-
-
-def convert_decimal(s):
-    return decimal.Decimal(s)
 
 
 def tz_aware_timestamp_adapter(val):
@@ -48,104 +42,95 @@ def tz_aware_timestamp_adapter(val):
 
 sqlite3.register_converter('timestamp', tz_aware_timestamp_adapter)
 sqlite3.register_adapter(decimal.Decimal, adapt_decimal)
-# sqlite3.register_converter("DECIMAL", convert_decimal)
 
 
 class EthereumDatabase(object):
 
-    def __init__(self, db_filepath=DB_FILEPATH):
-        self.db_filepath = db_filepath
-        self.conn = sqlite3.connect(
-            self.db_filepath, detect_types=sqlite3.PARSE_DECLTYPES)
-        self.conn.row_factory = sqlite3.Row
-        self.cur = self.conn.cursor()
+    def __init__(self, db_filepath):
+        self._db_filepath = db_filepath
+        self._table_name = "traces"
+
+        self._conn = sqlite3.connect(
+            self._db_filepath, detect_types=sqlite3.PARSE_DECLTYPES)
+        self._conn.row_factory = sqlite3.Row
 
     def __del__(self):
-        self.conn.close()
+        self._conn.close()
+
+    @property
+    def table_name(self):
+        return self._table_name
 
     def database_create(self):
-        self.cur.execute("""
-            CREATE TABLE traces(
-                transaction_hash TEXT,
-                transaction_index INT,
-                from_address TEXT,
-                to_address TEXT,
-                value DECIMAL,
-                input TEXT,
-                output TEXT,
-                trace_type TEXT NOT NULL,
-                call_type TEXT,
-                reward_type TEXT,
-                gas INT,
-                gas_used INT,
-                subtraces INT,
-                trace_address TEXT,
-                error TEXT,
-                status INT,
-                block_timestamp TIMESTAMP NOT NULL,
-                block_number INT NOT NULL,
-                block_hash STRING NOT NULL
-            );
-        """)
+        with self._conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE traces(
+                    transaction_hash TEXT,
+                    transaction_index INT,
+                    from_address TEXT,
+                    to_address TEXT,
+                    value DECIMAL,
+                    input TEXT,
+                    output TEXT,
+                    trace_type TEXT NOT NULL,
+                    call_type TEXT,
+                    reward_type TEXT,
+                    gas INT,
+                    gas_used INT,
+                    subtraces INT,
+                    trace_address TEXT,
+                    error TEXT,
+                    status INT,
+                    block_timestamp TIMESTAMP NOT NULL,
+                    block_number INT NOT NULL,
+                    block_hash STRING NOT NULL
+                );
+            """)
 
-        self.cur.execute("""
-            CREATE TABLE subtraces(
-                transaction_hash TEXT,
-                id INT PRIMARY KEY,
-                parent_trace_id INT
-            );
-        """)
-
-        self.cur.execute("""
-            CREATE TABLE crawl_records(
-                from_time TIMESTAMP NOT NULL,
-                to_time TIMESTAMP NOT NULL,
-                trace_count INT NOT NULL
-            );
-        """)
+            cur.execute("""
+                CREATE TABLE subtraces(
+                    transaction_hash TEXT,
+                    id INT PRIMARY KEY,
+                    parent_trace_id INT
+                );
+            """)
 
     def database_index_create(self):
         """
         This should ONLY be called after all data is crawled.
         """
-        self.cur.execute(
-            "CREATE INDEX transaction_hash_index ON traces(transaction_hash);")
-        # self.cur.execute(
-        #     "CREATE INDEX block_timestamp_index ON traces(block_timestamp);")
-        self.cur.execute(
-            "CREATE INDEX subtraces_transaction_hash_index ON subtraces(transaction_hash);")
+        with self._conn.cursor() as cur:
+            cur.execute(
+                "CREATE INDEX transaction_hash_index ON traces(transaction_hash);")
 
-        self.database_commit()
+            cur.execute(
+                "CREATE INDEX subtraces_transaction_hash_index ON subtraces(transaction_hash);")
 
-    def database_insert(self, rows):
-        # self.cur.executemany("""
-        #     INSERT INTO traces(transaction_hash, transaction_index, from_address, to_address, value, input, output, trace_type, call_type, reward_type, gas, gas_used, subtraces, trace_address, error, status, block_timestamp, block_number, block_hash)
-        #     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-        # """, rows)
+            self.database_commit()
 
+    def database_insert(self, rows, show_progress=False):
         trace_count = 0
-        for row in rows:
-            # import IPython;IPython.embed()
-            try:
-                self.cur.execute("""
-                    INSERT INTO traces(transaction_hash, transaction_index, from_address, to_address, value, input, output, trace_type, call_type, reward_type, gas, gas_used, subtraces, trace_address, error, status, block_timestamp, block_number, block_hash)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-                """, row)
-            except sqlite3.Error as e:
-                print(e)
-            trace_count += 1
-            sys.stdout.write(str(trace_count) + '\r')
-            sys.stdout.flush()
+
+        with self._conn.cursor() as cur:
+            for row in rows:
+                try:
+                    cur.execute("""
+                        INSERT INTO traces(transaction_hash, transaction_index, from_address, to_address, value, input, output, trace_type, call_type, reward_type, gas, gas_used, subtraces, trace_address, error, status, block_timestamp, block_number, block_hash)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+                    """, row)
+                    trace_count += 1
+                except sqlite3.Error as e:
+                    l.error("database insertion failed with error %s", e)
+                    return trace_count
+
+                if show_progress:
+                    sys.stdout.write(str(trace_count) + '\r')
+                    sys.stdout.flush()
+
         return trace_count
 
     def database_commit(self):
-        self.conn.commit()
-
-    def update_crawl_records(self, from_time, to_time, trace_count):
-        self.cur.execute("""
-            INSERT INTO crawl_records(from_time, to_time, trace_count)
-            VALUES (?, ?, ?)
-        """, (from_time, to_time, trace_count))
+        self._conn.commit()
 
     def read_from_database(self):
         '''
