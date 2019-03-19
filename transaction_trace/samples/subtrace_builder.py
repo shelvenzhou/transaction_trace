1c3a3834ed20b9426e90a14c5295c909d98b6004
Shelven Zhou <favoring.zhou49@gmail.com>
2019-03-19 09:46:32 +0800

    add subtrace_builder script

---
+++ transaction_trace/samples/subtrace_builder.py
@@ -0,0 +1,17 @@
+import sys
+
+import transaction_trace
+from transaction_trace.analysis import SubtraceBuilder
+
+
+def main(db_folder, from_time, to_time):
+    subtrace_builder = SubtraceBuilder(db_folder)
+    subtrace_builder.build_subtrace(from_time, to_time)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: %s db_folder from_time to_time" % sys.argv[0])
+        exit(-1)
+
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
