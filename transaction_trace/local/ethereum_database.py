0ce16804c7ffa35b2d976e70f8b78a58e8e12a51
stillkam <stillkammy@gmail.com>
2019-04-01 20:55:52 +0800

    refactor

--- transaction_trace/local/ethereum_database.py
+++ transaction_trace/local/ethereum_database.py
@@ -1,159 +1,19 @@
 import datetime
-import decimal
 import logging
 import os
 import re
-import sqlite3
-import sys
 
 from sortedcontainers import SortedList
 
-from ..datetime_utils import date_to_str, str_to_date, str_to_time
-from .database import Database
+from ..datetime_utils import date_to_str, str_to_date
+from .single_database import SingleTraceDatabase
 
 l = logging.getLogger("transaction-trace.local.ethereum_database")
 
 
-def adapt_decimal(d):
-    return str(d)
 
-
-def tz_aware_timestamp_adapter(val):
-    datepart, timepart = val.split(b" ")
-    year, month, day = map(int, datepart.split(b"-"))
-
-    if b"+" in timepart:
-        timepart, tz_offset = timepart.rsplit(b"+", 1)
-        if tz_offset == b'00:00':
-            tzinfo = datetime.timezone.utc
-        else:
-            hours, minutes = map(int, tz_offset.split(b':', 1))
-            tzinfo = datetime.timezone(
-                datetime.timedelta(hours=hours, minutes=minutes))
-    else:
-        tzinfo = datetime.timezone.utc
-
-    timepart_full = timepart.split(b".")
-    hours, minutes, seconds = map(int, timepart_full[0].split(b":"))
-
-    if len(timepart_full) == 2:
-        microseconds = int('{:0<6.6}'.format(timepart_full[1].decode()))
-    else:
-        microseconds = 0
-
-    val = datetime.datetime(year, month, day, hours,
-                            minutes, seconds, microseconds, tzinfo)
-
-    return val
-
-
-sqlite3.register_converter('timestamp', tz_aware_timestamp_adapter)
-sqlite3.register_adapter(decimal.Decimal, adapt_decimal)
-
-
-class SingleDatabase(Database):
-    def __init__(self, db_filepath, date):
-        super(SingleDatabase, self).__init__(db_filepath)
-
-        self._date = date
-
-    @property
-    def date(self):
-        return self._date
-
-    def create_traces_table(self):
-        cur = self._conn.cursor()
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS traces(
-                transaction_hash TEXT,
-                transaction_index INT,
-                from_address TEXT,
-                to_address TEXT,
-                value DECIMAL,
-                input TEXT,
-                output TEXT,
-                trace_type TEXT NOT NULL,
-                call_type TEXT,
-                reward_type TEXT,
-                gas INT,
-                gas_used INT,
-                subtraces INT,
-                trace_address TEXT,
-                error TEXT,
-                status INT,
-                block_timestamp TIMESTAMP NOT NULL,
-                block_number INT NOT NULL,
-                block_hash STRING NOT NULL
-            );
-        """)
-
-    def create_subtraces_table(self):
-        cur = self._conn.cursor()
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS subtraces(
-                transaction_hash TEXT,
-                trace_id INT PRIMARY KEY,
-                parent_trace_id INT
-            );
-        """)
-
-    def insert_traces(self, rows):
-        """
-        Manual database commit is needed.
-        """
-        self.insert(
-            "traces",
-            "transaction_hash, transaction_index, from_address, to_address, value, input, output, trace_type, call_type, reward_type, gas, gas_used, subtraces, trace_address, error, status, block_timestamp, block_number, block_hash",
-            "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?",
-            rows
-        )
-
-    def read_traces(self, with_rowid=False):
-        '''
-            'transaction_hash':     STRING,     NULLABLE
-            'transaction_index':    INTEGER,    NULLABLE
-            'from_address':         STRING,     NULLABLE
-            'to_address':           STRING,     NULLABLE
-            'value':                DECIMAL,    NULLABLE
-            'input':                STRING,     NULLABLE
-            'output':               STRING,     NULLABLE
-            'trace_type':           STRING,     REQUIRED
-            'call_type':            STRING,     NULLABLE
-            'reward_type':          STRING,     NULLABLE
-            'gas':                  INTEGER,    NULLABLE
-            'gas_used':             INTEGER,    NULLABLE
-            'subtraces':            INTEGER,    NULLABLE
-            'trace_address':        STRING,     NULLABLE
-            'error':                STRING,     NULLABLE
-            'status':               INTEGER,    NULLABLE
-            'block_timestamp':      TIMESTAMP,  REQUIRED
-            'block_number':         INTEGER,    REQUIRED
-            'block_hash':           STRING,     REQUIRED
-        '''
-        columns = "rowid, *" if with_rowid else "*"
-        return self.read("traces", columns)
-
-    def insert_subtrace(self, row):
-        """
-        Manual database commit is needed.
-        """
-        self.insert(
-            "subtraces",
-            "transaction_hash, trace_id, parent_trace_id",
-            "?, ?, ?",
-            row
-        )
-
-    def read_subtraces(self, with_rowid=False):
-        columns = "rowid, *" if with_rowid else "*"
-        return self.read("subtraces", columns)
-
-    def clear_subtraces(self):
-        self.delete("subtraces")
-
-
-def data_time_range(db_folder):
-    prog = re.compile(r"bigquery_ethereum_(\d{4}\-\d{2}\-\d{2})\.sqlite3")
+def data_time_range(db_folder, db_name):
+    prog = re.compile(r"%s_(\d{4}\-\d{2}\-\d{2})\.sqlite3" % db_name)
     dates = SortedList(key=lambda x: str_to_date(x))
     for file in os.listdir(db_folder):
         m = prog.match(file)
@@ -163,16 +23,17 @@ def data_time_range(db_folder):
     return dates
 
 
-def db_filename(date):
-    return "bigquery_ethereum_%s.sqlite3" % date
+def db_filename(db_name, date):
+    return "%s_%s.sqlite3" % (db_name, date)
 
 
 class EthereumDatabase:
 
     def __init__(self, db_folder, cache_capacity=20):
         self._db_folder = db_folder
+        self._db_name = db_folder.split("_")[-1]
 
-        self._data_time_range = data_time_range(db_folder)
+        self._data_time_range = data_time_range(db_folder, self._db_name)
 
         self._cache_capacity = cache_capacity
         self._connection_cache = dict()
@@ -202,8 +63,8 @@ class EthereumDatabase:
             conn = self._connection_cache.pop(lru)
             del conn
 
-        db_filepath = os.path.join(self._db_folder, db_filename(date))
-        db = SingleDatabase(db_filepath, date)
+        db_filepath = os.path.join(self._db_folder, db_filename(self._db_name, date))
+        db = SingleTraceDatabase(db_filepath, date)
         self._connection_access.append(date)
         self._connection_cache[date] = db
         return db
--- transaction_trace/remote/ethereum_bigquery.py
+++ transaction_trace/remote/ethereum_bigquery.py
@@ -13,11 +13,14 @@ class EthereumBigQuery(RemoteDateSource):
     def __init__(self):
         self.client = bigquery.Client()
 
-    def get_ethereum_data(self, from_time, to_time):
+    def get_ethereum_data(self, from_time, to_time, db_name):
+        if db_name == "blocks":
+            timestamp = "timestamp"
+        else:
+            timestamp = "block_timestamp"
         query_str = (
-            # f'SELECT * FROM `bigquery-public-data.ethereum_blockchain.blocks` '
-            f'SELECT * FROM `bigquery-public-data.ethereum_blockchain.transactions` '
-            f'WHERE block_timestamp >= "{time_to_str(from_time)}" AND block_timestamp < "{time_to_str(to_time)}"'
+            f'SELECT * FROM `bigquery-public-data.ethereum_blockchain.{db_name}` '
+            f'WHERE {timestamp} >= "{time_to_str(from_time)}" AND {timestamp} < "{time_to_str(to_time)}"'
         )
 
         return self.client.query(query_str).result()
