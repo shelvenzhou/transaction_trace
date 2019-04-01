7248be5a006ffcf80d2883b52b966f1daddb4c84
stillkam <stillkammy@gmail.com>
2019-04-01 20:23:18 +0800

    refactor

--- transaction_trace/samples/crawler.py
+++ transaction_trace/samples/crawler.py
@@ -1,11 +1,12 @@
 import logging
 from datetime import datetime, timedelta
 import sqlite3
+import sys
+import os
 
-import transaction_trace
-from transaction_trace.datetime_utils import date_to_str, str_to_time, time_to_str
-from transaction_trace.local.database import Database
-from transaction_trace.local.ethereum_database import SingleDatabase
+from transaction_trace.datetime_utils import date_to_str, str_to_time, time_to_str, str_to_date
+from transaction_trace.local.single_database import *
+from transaction_trace.local.ethereum_database import db_filename
 from transaction_trace.remote.ethereum_bigquery import EthereumBigQuery
 
 l = logging.getLogger("transaction-trace.utilities.crawler")
@@ -13,89 +14,75 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def main():
-    remote = EthereumBigQuery()
+database_map = {
+    "traces": {
+        "class_name": "SingleTraceDatabase",
+        "create": "create_traces_table",
+        "insert": "insert_traces"
+    },
+    "blocks": {
+        "class_name": "SingleBlockDatabase",
+        "create": "create_blocks_table",
+        "insert": "insert_blocks"
+    },
+    "transactions": {
+        "class_name": "SingleTransactionDatabase",
+        "create": "create_txs_table",
+        "insert": "insert_txs"
+    }
+}
 
+
+def main(db_folder, crawl_time_path, time_interval, to_time, from_time):
+    remote = EthereumBigQuery()
+    db_name = db_folder.split("_")[-1]
     # data insertion
     try:
-        with open("/home/xiangjie/logs/crawl-time", "r") as f:
+        with open(crawl_time_path, "r") as f:
             from_time = str_to_time(f.readline())
     except:
-        from_time = datetime(2015, 8, 7, 0, 0, 0)
+        if from_time == None:
+            print("from_time not set")
+            exit(-1)
+        from_time = str_to_date(from_time)
+    t_time = from_time + timedelta(hours=int(time_interval))
 
-    to_time = from_time + timedelta(hours=1)
-
-    while from_time < datetime(2019, 3, 21, 0, 0, 0):
+    while from_time < str_to_date(to_time):
         date = from_time.date()
-        local = Database(
-            f"/mnt/data/bigquery/ethereum_transactions/transactions_{date_to_str(date)}.sqlite3")
-        # local = SingleDatabase(f"/mnt/data/bigquery/ethereum_blocks/bigquery_ethereum_{date_to_str(date)}.sqlite3", date_to_str(date))
+        date_str = date_to_str(date)
+        db_filepath = os.path.join(db_folder, db_filename(db_name, date_str))
+        # import IPython;IPython.embed()
+        db = globals()[database_map[db_name]["class_name"]](db_filepath, date_str)
         try:
-            # local.create_table(table_name="blocks", columns='''(
-            #     timestamp TIMESTAMP NOT NULL,
-            #     number INT NOT NULL,
-            #     hash TEXT NOT NULL,
-            #     parent_hash TEXT,
-            #     nonce TEXT NOT NULL,
-            #     sha3_uncles TEXT,
-            #     logs_bloom TEXT,
-            #     transactions_root TEXT,
-            #     state_root TEXT,
-            #     receipts_root TEXT,
-            #     miner TEXT,
-            #     difficulty INT,
-            #     total_difficulty INT,
-            #     size INT,
-            #     extra_data TEXT,
-            #     gas_limit INT,
-            #     gas_used INT,
-            #     transaction_count INT
-            #     )''')
-            local.create_table(table_name="transactions", columns='''(
-                hash TEXT PRIMARY KEY,
-                nonce INT NOT NULL,
-                transaction_index INT NOT NULL,
-                from_address TEXT NOT NULL,
-                to_address TEXT,
-                value INT,
-                gas INT,
-                gas_price INT,
-                input TEXT,
-                receipt_cumulative_gas_used INT,
-                receipt_gas_used INT,
-                receipt_contract_address TEXT,
-                receipt_root TEXT,
-                receipt_status INT,
-                block_timestamp TIMESTAMP NOT NULL,
-                block_number INT NOT NULL,
-                block_hash TEXT NOT NULL
-                )''')
-            # local.create_traces_table()
+            getattr(db, database_map[db_name]["create"])()
         except sqlite3.Error as e:
             print(e)
 
-        print(f"date:", date_to_str(date))
+        print(f"date:", date_str)
         while from_time.date() == date:
 
-            print(f"query from {from_time} to {to_time}...")
-            rows = remote.get_ethereum_data(from_time, to_time)
+            print(f"query from {from_time} to {t_time}...")
+            rows = remote.get_ethereum_data(from_time, t_time, db_name)
             count = 0
             for row in rows:
-                # local.insert(table="blocks", 
-                #     columns="(timestamp, number, hash, parent_hash, nonce, sha3_uncles, logs_bloom, transactions_root, state_root, receipts_root, miner, difficulty, total_difficulty, size, extra_data, gas_limit, gas_used, transaction_count)", 
-                #     placeholders="?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?", rows=row)
-                local.insert(table="transactions", columns="", placeholders="?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?", rows=row)
-                # local.insert_traces(row)
+                getattr(db, database_map[db_name]["insert"])(row)
                 count += 1
             print(count, "txs")
-            # local.update_crawl_records(from_time, to_time, trace_count)
-            local.commit()
+            db.commit()
 
-            from_time = to_time
-            to_time += timedelta(hours=1)
-            with open("/home/xiangjie/logs/crawl-time", "w+") as f:
+            from_time = t_time
+            t_time += timedelta(hours=1)
+            with open(crawl_time_path, "w+") as f:
                 f.write(time_to_str(from_time))
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 5:
+        print("Usage: python3 %s db_folder crawl_time_path time_interval to_time [from_time]" %
+              sys.argv[0])
+        exit(-1)
+
+    if len(sys.argv) == 5:
+        sys.argv[5] = None
+
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
