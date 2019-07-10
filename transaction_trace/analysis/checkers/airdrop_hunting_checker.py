3fcac725a9217be1e7677ba940b14553959c42a9
Shelven Zhou <favoring.zhou49@gmail.com>
2019-07-10 19:16:02 +0800

    comment AirdropHuntingChecker and fix bugs

--- transaction_trace/analysis/checkers/airdrop_hunting_checker.py
+++ transaction_trace/analysis/checkers/airdrop_hunting_checker.py
@@ -1,61 +1,62 @@
-from .checker import Checker, CheckerType
-from ..intermediate_representations import ResultGraph, ResultType
 from collections import defaultdict
+
+from ..intermediate_representations import ResultGraph, ResultType
+from .checker import Checker, CheckerType
 
 
 class AirdropHuntingChecker(Checker):
 
-    def __init__(self, threshold):
+    def __init__(self):
         super(AirdropHuntingChecker, self).__init__("airdrop-hunting")
-        self.threshold = threshold
 
     @property
     def checker_type(self):
         return CheckerType.TRANSACTION_CENTRIC
 
     def check_transaction(self, action_tree, result_graph):
-        candidates = list()
+        tx = action_tree.tx
+        at = action_tree.t
+        rg = result_graph.g
+
+        slaves = list()
         # search for airdrop-hunting candidates edge by edge
-        edges = action_tree.t.edges()
-        if len(edges) < self.threshold:
-            return
+        edges = at.edges()
         for e in edges:
-            trace = action_tree.t.edges[e]
+            trace = at.edges[e]
             # add the created slave contract to candidates
             if trace['trace_type'] == 'create':
-                candidates.append(e[1])
+                slaves.append(e[1])
 
-        tx = action_tree.tx
-        hunting_time = list()
+        huntings = list()
         # search partial-result-graph for each candidate
-        for n in candidates:
-            prg = ResultGraph.build_partial_result_graph(result_graph.t, n)
+        for s in slaves:
+            prg = ResultGraph.build_partial_result_graph(at, s)
 
             results = list()
             for node in prg.nodes():
                 for result_type in prg.nodes[node]:
-                    if ResultGraph.extract_result_type(result_type) != ResultType.TOKEN_TRANSFER:
-                        continue
-                    if prg.nodes[node][result_type] > 0:
-                        results.append({
-                            "profit_node": node,
-                            "result_type": result_type,
-                            "amount": prg.nodes[node][result_type]
-                        })
+                    # in airdrop hunting, we only concern about token transfer
+                    if ResultGraph.extract_result_type(result_type) == ResultType.TOKEN_TRANSFER:
+                        amount = prg.nodes[node][result_type]
+                        if amount > 0:
+                            results.append({
+                                "profit_node": node,
+                                "result_type": result_type,
+                                "amount": amount,
+                            })
 
             if len(results) > 0:
-                hunting_time.append(results)
+                huntings.append(results)
 
-        if len(hunting_time) > self.threshold:
-            rg = result_graph
+        if len(huntings) > 0:
             profits = dict()
-            for node in rg.g.nodes():
+            for node in rg.nodes():
                 profit = dict()
-                if result_type in rg.g.nodes[node]:
+                for result_type in rg.nodes[node]:
                     if ResultGraph.extract_result_type(result_type) != ResultType.TOKEN_TRANSFER_EVENT:
                         continue
-                    if rg.g.nodes[node][result_type] > self.minimum_profit_amount[ResultType.TOKEN_TRANSFER]:
-                        profit[result_type] = rg.g.nodes[node][result_type]
+                    if rg.nodes[node][result_type] > self.minimum_profit_amount[ResultType.TOKEN_TRANSFER]:
+                        profit[result_type] = rg.nodes[node][result_type]
                 if len(profit) > 0:
                     profits[node] = profit
 
@@ -63,6 +64,6 @@ class AirdropHuntingChecker(Checker):
                 tx.is_attack = True
                 tx.attack_details.append({
                     "checker": self.name,
-                    "hunting_time": len(hunting_time),
+                    "hunting_time": len(huntings),
                     "profit": profits
                 })
