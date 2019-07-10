b9676bf4f1f4c2e894f40b3357d8d5d9d7d5c6b0
Shelven Zhou <favoring.zhou49@gmail.com>
2019-07-10 19:14:44 +0800

    move AttackCandidate

--- transaction_trace/analysis/intermediate_representations/__init__.py
+++ transaction_trace/analysis/intermediate_representations/__init__.py
@@ -2,13 +2,4 @@ from .result_type import ResultType
 from .action_tree import ActionTree, extract_address_from_node, extract_trace_id_from_node, get_edges_from_cycle
 from .result_graph import ResultGraph
 from .transaction import Transaction
-
-__all__ = [
-    'ResultType',
-    'ActionTree',
-    'extract_address_from_node',
-    'extract_trace_id_from_node',
-    'get_edges_from_cycle',
-    'ResultGraph',
-    'Transaction'
-]
+from .attack_candidate import AttackCandidate
