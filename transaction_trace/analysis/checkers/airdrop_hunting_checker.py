bb8f1ddc5a1fc5a38077eebcf9002d3d0e13bec0
Shelven Zhou <favoring.zhou49@gmail.com>
2019-07-15 08:31:39 +0800

    remove delegatecall from call injection check

--- transaction_trace/analysis/checkers/airdrop_hunting_checker.py
+++ transaction_trace/analysis/checkers/airdrop_hunting_checker.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-from ..intermediate_representations import ResultGraph, ResultType
+from ..intermediate_representations import ResultGraph, ResultType, AttackCandidate
 from .checker import Checker, CheckerType
 
 
@@ -19,9 +19,7 @@ class AirdropHuntingChecker(Checker):
         rg = result_graph.g
 
         slaves = list()
-        # search for airdrop-hunting candidates edge by edge
-        edges = at.edges()
-        for e in edges:
+        for e in at.edges():
             trace = at.edges[e]
             # add the created slave contract to candidates
             if trace['trace_type'] == 'create':
@@ -62,8 +60,13 @@ class AirdropHuntingChecker(Checker):
 
             if len(profits) > 0:
                 tx.is_attack = True
-                tx.attack_details.append({
-                    "checker": self.name,
-                    "hunting_time": len(huntings),
-                    "profit": profits
-                })
+                tx.attack_details.append(
+                    AttackCandidate(
+                        self.name,
+                        {
+                            "transaction": tx.tx_hash,
+                            "hunting_time": len(huntings)
+                        },
+                        profits,
+                    )
+                )
--- transaction_trace/analysis/checkers/call_injection_checker.py
+++ transaction_trace/analysis/checkers/call_injection_checker.py
@@ -1,6 +1,5 @@
-from ..intermediate_representations import (ActionTree, ResultGraph,
-                                            ResultType,
-                                            extract_address_from_node)
+from ..intermediate_representations import (
+    ActionTree, ResultGraph, ResultType, extract_address_from_node, AttackCandidate)
 from ..knowledge import SensitiveAPIs, extract_function_signature
 from .checker import Checker, CheckerType
 
@@ -52,31 +51,28 @@ class CallInjectionChecker(Checker):
                 parent_input = parent_trace['input']
                 # TODO: not consider fallback function in "call" may cause FN, but also reduce FP on same func-name
                 if len(parent_input) > 10:
-                    if called_func[2:] in (parent_input if trace['call_type'] == "delegatecall" else parent_input[10:]):
+                    if called_func[2:] in parent_input[10:]:
                         input_control = True
                     else:
                         encoded_functions = SensitiveAPIs.encoded_functions()
                         for t in encoded_functions:
                             if callee in encoded_functions[t]:
                                 encoded_callee = encoded_functions[t][callee]
-                                if encoded_callee in parent_trace_input[10:]:
+                                if encoded_callee in parent_input[10:]:
                                     input_control = True
 
                 if input_control:
                     candidates.append((e, parent_edge))
 
+        # TODO: we've already remove `delegatecall` cases, is this still necessary?
         attacks = list()
         sensitive_nodes = set()
         # search partial-result-graph for each candidate
         for (e, parent_edge) in candidates:
-            ancestors = ActionTree.get_ancestors_from_tree(action_tree.t, e[0])
-            call_type = action_tree.t.edges[e]['call_type']
+            ancestors = ActionTree.get_ancestors_from_tree(at, e[0])
+            call_type = at.edges[e]['call_type']
 
-            # only consider the direct trace result graph when "delegatecall"
-            direct_trace = True if call_type == "delegatecall" else False
-            prg = ResultGraph.build_partial_result_graph(
-                result_graph.t, e[0], direct_trace)
-
+            prg = ResultGraph.build_partial_result_graph(result_graph.t, e[0])
             results = dict()
             for e in prg.edges():
                 if e[1] not in ancestors:
@@ -100,19 +96,18 @@ class CallInjectionChecker(Checker):
 
             if len(results) > 0:
                 attacks.append({
-                    "edge": parent_edge,
+                    "entry_edge": parent_edge,
                     'results': results
                 })
 
         if len(attacks) > 0:
             # compute whole transaction economic lost
-            rg = result_graph
             profits = dict()
-            for node in rg.g.nodes():
+            for node in rg.nodes():
                 if node not in sensitive_nodes:
                     continue
                 profit = dict()
-                for result_type in rg.g.nodes[node]:
+                for result_type in rg.nodes[node]:
                     rt = ResultGraph.extract_result_type(result_type)
                     if rt == ResultType.OWNER_CHANGE:
                         profit[result_type] = None
@@ -127,8 +122,13 @@ class CallInjectionChecker(Checker):
 
             if len(profit) > 0:
                 tx.is_attack = True
-                tx.attack_details.append({
-                    "checker": self.name,
-                    "attacks": attacks,
-                    "profit": profits
-                })
+                tx.attack_details.append(
+                    AttackCandidate(
+                        self.name,
+                        {
+                            "transaction": tx.tx_hash,
+                            "attacks": attacks,
+                        },
+                        profits,
+                    )
+                )
